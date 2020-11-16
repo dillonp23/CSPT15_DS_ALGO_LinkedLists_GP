2caf7ecfd07ee63e6c5d08af181201d328531a12
Dillon <dillonp.lambda@gmail.com>
2020-11-15 18:22:51 -0800

    Added a simple solution for first demo problem - consider this solution only utilizies a Node class based on the context of this problem and does not traverse a LinkedList as this is outisde the scope of what is asked of us.

--- src/demos/demo_1.py
+++ src/demos/demo_1.py
@@ -23,6 +23,16 @@ class LinkedListNode():
 
 def delete_node(delete_this_node):
     # Your code here
+    next = delete_this_node.next
+
+    if next:
+        delete_this_node.value = next.value
+        delete_this_node.next = next.next
+
+    else:
+        # We're at the end of the list so write an exception (just for the purposes of this demo we're only deleting within the middle of a list)
+        raise Exception("This technique does not work for the last node of the list")
+
     pass
 
 
@@ -33,4 +43,22 @@ z = LinkedListNode('Z')
 x.next = y
 y.next = z
 
+print(x)
+print(x.value)
+print(x.next.value)
+
+print(y)
+print(y.value)
+print(y.next.value)
+
+print(z)
+print(z.value)
+
 delete_node(y)
+
+print(x)
+print(x.value)
+print(x.next.value)
+
+print(y)
+print(y.value)
