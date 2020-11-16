0ae7b1d2d1bb7684e1a055ab18481037145790b5
Dillon <dillonp.lambda@gmail.com>
2020-11-15 17:51:07 -0800

    Added test code to create a new Node object and initialize a new LikedList object with it. Added append methods and print statements to ensure LinkedList and Node classes/objects are functioning as expected

--- src/demos/scratchpad.py
+++ src/demos/scratchpad.py
@@ -15,3 +15,31 @@ class Node:
 class LinkedList:
     def __init__(self, head=None):
         self.head = head
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head:
+            current = self.head
+
+            while current.next:
+                current = current.next
+
+            current.next = new_node
+        else:
+            self.head = new_node
+
+
+a = Node(1)
+llist = LinkedList(a)
+llist.append(2)
+llist.append(3)
+
+print(llist.head)
+print(llist.head.data)
+
+print(llist.head.next)
+print(llist.head.next.data)
+
+print(llist.head.next.next)
+print(llist.head.next.next.data)
