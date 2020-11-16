e1211bd8b1174ba251fae4293ad28a0a5ed9378a
Dillon <dillonp.lambda@gmail.com>
2020-11-15 17:41:00 -0800

    Added first steps of creating a LinkedList Node class to scratchpad.py

--- src/demos/scratchpad.py
+++ src/demos/scratchpad.py
@@ -1,3 +1,12 @@
 """
 Here we will outline a few concepts in comments and code.
 """
+
+# Creating linked lists in python:
+
+# 1. Start with a Node class
+class Node:
+    # Node initializer
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
